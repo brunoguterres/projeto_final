56e5f474077566ee22f00e159dbae04d12b3fdc4
brunoguterres <brunoguterres26@gmail.com>
2023-11-06 23:26:31 -0300

    carregando ottotrechos e estilizando

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 function onEachFeature(feature, layer) {
     layer.on('click', function (e) {
         var cobaciaValue = feature.properties.cobacia;
+        console.log("Tipo de cobaciaValue: " + typeof cobaciaValue);
 
         var cobaciaEdit = cobaciaValue;
         while (cobaciaEdit.length > 0 && parseInt(cobaciaEdit.slice(-1)) % 2 !== 0) {
             cobaciaEdit = cobaciaEdit.slice(0, -1); // Remove o último dígito
         };
     
-        var popupContent = "CobaciaValue: " + cobaciaValue + "<br>CobaciaEdit: " + cobaciaEdit;
+        var popupContent = "CobaciaValue: "+cobaciaValue+"<br>CobaciaEdit: "+cobaciaEdit;
         
         L.popup()
             .setLatLng(e.latlng)
@@ -18,7 +19,7 @@ function onEachFeature(feature, layer) {
             layers: 'teste_shapefile:ottobacias_iguacu_5k',
             fitLayer: false,
             className: 'camada_ottobacias_montante',
-            CQL_FILTER: "cobacia LIKE '"+cobaciaEdit+"%' AND cobacia > '"+cobaciaValue+"'"
+            CQL_FILTER: "cobacia LIKE '"+cobaciaEdit+"%' AND cobacia > '"+cobaciaValue+"';"
         });
         ottobaciasMontante.addTo(map);
         
@@ -26,7 +27,7 @@ function onEachFeature(feature, layer) {
             layers: 'teste_shapefile:ottobacias_iguacu_5k',
             fitLayer: false,
             className: 'camada_ottobacias_selecionada',
-            CQL_FILTER: "cobacia LIKE '"+cobaciaValue+"'"
+            CQL_FILTER: "cobacia LIKE '"+cobaciaValue+"';"
         });
         ottobaciaSelecionada.addTo(map);
     });
@@ -50,10 +51,9 @@ var baseOpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.
 
 var ottotrechos = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
     layers: 'teste_shapefile:ottotrechos_iguacu_5k',
-    //className: 'camada_ottobacias',
+    className: 'camada_ottotrechos',
     attribution: 'ANA',
-    CQL_FILTER: "cobacia LIKE '86288%'",
-    //onEachFeature: onEachFeature
+    CQL_FILTER: "cobacia LIKE '8628%'"
 });
 
 var ottobacias = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
@@ -67,7 +67,7 @@ var ottobacias = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
 var map = L.map('map', {
     center: [-15, -51.5],
     zoom: 4,
-    layers: [baseGoogleStreets, ottobacias]
+    layers: [baseGoogleStreets, ottobacias, ottotrechos]
 });
 
 var baseMaps = {
