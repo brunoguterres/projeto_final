65543f1d5789964c9c97db17f6e6bece7b7a6999
brunoguterres <brunoguterres26@gmail.com>
2023-11-08 17:55:26 -0300

    alterando dados IG6

--- scripts.js
+++ scripts.js
@@ -4,71 +4,75 @@ function onEachFeature(feature, layer) {
         var cobaciaEdit = cobaciaValue;
 
         while (cobaciaEdit.length > 0 && parseInt(cobaciaEdit.slice(-1)) % 2 !== 0) {
-            cobaciaEdit = cobaciaEdit.slice(0, -1); // Remove o último dígito
+            cobaciaEdit = cobaciaEdit.slice(0, -1);
         };
 
-        var popupContent = "CobaciaValue: "+cobaciaValue+"<br>CobaciaEdit: "+cobaciaEdit;
+        var popupContent = "CobaciaValue: "+cobaciaValue+typeof cobaciaValue+"<br>CobaciaEdit: "+cobaciaEdit+typeof cobaciaValue;
         L.popup()
             .setLatLng(e.latlng)
             .setContent(popupContent)
             .openOn(map);
         
         var ottobaciasMontante = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
-            layers: 'teste_shapefile:ottobacias_iguacu_5k',
+            layers: 'hidrogis:ottobacias_AI_IG6_ISR',
             fitLayer: false,
             className: 'camada_ottobacias_montante',
-            CQL_FILTER: "cobacia LIKE '"+cobaciaEdit+"%' AND cobacia > '"+cobaciaValue+"';"
+            CQL_FILTER: "cobacia LIKE '"+cobaciaEdit+"%' AND cobacia >= '"+cobaciaValue+"'"
         });
         ottobaciasMontante.addTo(map);
     });
 }
 
-var baseGoogleStreets = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    attribution: 'Google Streets © <a href="https://www.google.com/maps">Google Maps</a>'
+var baseOpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    opacity: 0.5,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
 var baseGoogleSatelite = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 22,
+    opacity: 0.5,
     attribution: 'Imagem de satélite © <a href="https://www.google.com/maps">Google Maps</a>'
 });
 
-var baseOpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var baseGoogleStreets = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    maxZoom: 20,
+    opacity: 0.5,
+    attribution: 'Google Streets © <a href="https://www.google.com/maps">Google Maps</a>'
 });
 
-var ottotrechos = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
-    layers: 'teste_shapefile:ottotrechos_iguacu_5k',
-    className: 'camada_ottotrechos',
-    attribution: 'ANA',
-    CQL_FILTER: "cobacia LIKE '8628%'"
-});
 
 var ottobacias = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
-    layers: 'teste_shapefile:ottobacias_iguacu_5k',
+    layers: 'hidrogis:ottobacias_AI_IG6_ISR',
     className: 'camada_ottobacias',
     attribution: 'ANA',
-    CQL_FILTER: "cobacia LIKE '8628%'",
     onEachFeature: onEachFeature
 });
+
+/*
+var ottotrechos = L.Geoserver.wfs('http://191.252.221.146:8080/geoserver/wfs', {
+    layers: 'hidrogis:ottotrechos_AI_IG6',
+    //className: 'camada_ottotrechos',
+    attribution: 'ANA'
+});
+*/
 
 var map = L.map('map', {
     center: [-15, -51.5],
     zoom: 4,
-    layers: [baseGoogleStreets, ottotrechos, ottobacias]
+    layers: [baseOpenStreetMap, /*ottotrechos,*/ ottobacias]
 });
 
 var baseMaps = {
-    "Google Streets": baseGoogleStreets,
+    "OpenStreetMap": baseOpenStreetMap,
     "Google Satelite": baseGoogleSatelite,
-    "OpenStreetMap": baseOpenStreetMap    
+    "Google Streets": baseGoogleStreets
 };
 
 var overlayMaps = {
-    "Ottobacias": ottobacias,
-    "Ottotrechos": ottotrechos
+    "Ottobacias": ottobacias
+    /*"Ottotrechos": ottotrechos*/
 };
 
 var layerControl = L.control.layers(baseMaps, overlayMaps);
