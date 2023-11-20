edcc047d1d9f601472be4847637710b218a74538
brunoguterres <brunoguterres26@gmail.com>
2023-11-20 10:26:15 -0300

    refatoração python

--- teste.py
+++ teste.py
@@ -1,8 +1,8 @@
 cod_otto_interesse = '86289161'
 
 #   método que seleciona os trechos a jusante do ponto de interesse
-rio = ['','','','','','']
-rios = 0
+rios = ['','','','','','']
+n_rio = 0
 
 # seleção dos rios a jusante
 for valor in range(len(cod_otto_interesse)):
@@ -14,29 +14,30 @@ for valor in range(len(cod_otto_interesse)):
     # fiz o teste para final par, pois do contrário dá erro, não entendi
     if impar == 0:
         codfim = cod_otto_interesse[:(len(cod_otto_interesse)-valor)]
-        rio[rios] = codfim
-        rios = rios + 1
+        rios[n_rio] = codfim
+        n_rio = n_rio + 1
         compri = len(codfim)
         if compri <= 4:    # teste para ver se no rio princila da bacia (rio 8628)
             break
 
-print('self.rios:', rios)
-print('self.rio:', rio)
+print('n_rio:', n_rio)
+print('rios:', rios)
 
 selecao = ''
-for elementos in rio:
+for elementos in rios:
     if elementos != '':
-        selecao = selecao + 'camada_ottotrechos.cocursodag LIKE \''+ elementos +'\' OR '
+        selecao = selecao + 'camada_ottotrechos.cocursodag = \''+ elementos +'\' OR '
     else:
         break
-    comp = len(selecao)
-    comp2 = comp - 3
-    print('self.selecao:', selecao)
-    sele2 = selecao [:comp2]
-    print('self.sele2:', sele2)
+comp = len(selecao)
+comp2 = comp - 3
+print('selecao:', selecao)
+sele2 = selecao [:comp2]
+print('sele2:', sele2)
 query_ottotrechos_jusante = '?query=SELECT camada_ottotrechos.cocursodag, camada_ottotrechos.geometry '\
                             'FROM camada_ottotrechos '\
                             'WHERE ('+ sele2 +')AND camada_ottotrechos.cobacia < \''+cod_otto_interesse+'\''
+print('query_ottotrechos_jusante:', query_ottotrechos_jusante)
 
 
 print('\n''-> Seleção de ottorechos a jusante realizado.')
